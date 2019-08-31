24d54ec43a33edbe61c369417b82466ac8baefc3
Richard Chan <rchan817@yahoo.com>
2019-08-31 11:49:09 -0700

    checkpoint-4-completed

--- terminal-commands.js
+++ terminal-commands.js
@@ -9,3 +9,19 @@ module.exports.ls = () => {
     console.log(filesToString);
   });
 };
+
+module.exports.touch = () => {
+  fs.writeFile('test_file.txt', "Content here means command worked!", 'utf8', (err) => {
+    if (err) throw err;
+
+    console.log('File created!');
+  });
+};
+
+module.exports.mkdir = () => {
+  fs.mkdir('./test-directory', (err) => {
+    if (err) throw err;
+
+    console.log('Directory created!');
+  });
+};
